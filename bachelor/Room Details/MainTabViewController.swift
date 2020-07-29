c012f505790c0bc1e5fba502bcefc0ad114de070
Philippe Weidmann <philweidmann@me.com>
2020-07-29 11:19:54 +0200

    Refactor

---
+++ bachelor/Room Details/MainTabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabViewController.swift
+//  bachelor
+//
+//  Created by Philippe Weidmann on 29.07.20.
+//  Copyright © 2020 Philippe Weidmann. All rights reserved.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
